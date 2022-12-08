f2a6b43feea3ab472542daea05e4c368b3fe3121
Olayinka Oniyide <oniyideolayinka97@gmail.com>
2022-12-09 00:12:32 +0100

    solutions to the file assignment

---
+++ file_assignment.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+folder1 = Path.home() / "my_folder"
+folder1.mkdir(exist_ok=True)
+# new_file = file / "file1.txt"
+# new_file.touch(exist_ok=True)
+docs = [
+    folder1 / "file1.txt",
+    folder1 / "file2.txt",
+    folder1 / "image1.png",
+    ]
+for file in docs:
+    file.touch(exist_ok=True)
+folder2 = Path.home()/"my_folder" / "image"
+folder2.mkdir(exist_ok=True)
+source = Path(r"C:\Users\ADMIN\my_folder\image1.png")
+print(source.exists())
+destination = Path(r"C:\Users\ADMIN\my_folder\image\image1.png")
+print(destination.exists())
+source.replace(destination)
+file_delete = Path(r"C:\Users\ADMIN\my_folder\file2.txt")
+file_delete.unlink()
